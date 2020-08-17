2944d165b7cfab35e7fff5a07380e1182176a94c
Anita <codingwithanita@gmail.com>
2020-08-17 12:01:54 -0700

    Integration Adjustments

--- src/app/iseasons-data.ts
+++ src/app/iseasons-data.ts
@@ -1,47 +1,11 @@
 export interface ISeasonsData {
-  name: [
-    {
-      string: any
-    }
-  ],
-  number: [
-    {
-      number
-    }
-  ],
-  id: [
-    {
-      number
-    }
-  ],
-  seasonId: [
-    {
-      number
-    }
-  ],
-  episodeNumber: [
-    {
-      number
-    }
-  ],
-  premiereDate: [
-    {
-      string
-    }
-  ],
-  episodeName: [
-    {
-      string
-    }
-  ],
-  episodeOrder: [
-    {
-      number
-    }
-  ],
-  summary: [
-    {
-      string
-    }
-  ]
+  name: string,
+  number: number,
+  id: number,
+  seasonId: number,
+  episodeNumber: number,
+  premiereDate: string,
+  episodeName: string,
+  episodeOrder: number,
+  summary: string
 }
